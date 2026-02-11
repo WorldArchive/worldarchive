78549f9a37fa78388f19e41ed1f3f4432b7f6602
WorldArchive <ancienteartharchive@gmail.com>
2026-02-10 19:52:34 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,6 +4,11 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  // Asegurar que las variables estén disponibles en build time
+  env: {
+    NOTION_TOKEN: process.env.NOTION_TOKEN,
+    NOTION_DATABASE_ID: process.env.NOTION_DATABASE_ID,
+  },
 }
 
 module.exports = nextConfig
